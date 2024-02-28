653303e65ab8b57761feb46324769eb46a4da448
Stepha Gonzalez <gstephaniegonz@gmail.com>
2024-02-28 15:48:35 -0500

    Initial commit: Added main files and dependencies

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const { src, dest, watch } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+function css(done) {
+  //identificar archivo sass
+  //Compilarlo
+  //almacenarla en el disco duro
+  src("src/scss/**/*.scss").pipe(sass()).pipe(dest("build/css"));
+
+  done();
+}
+function dev(done) {
+  watch("src/scss/**/*.scss", css);
+  done();
+}
+exports.css = css;
+exports.dev = dev;
